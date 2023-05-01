e216804344502fb126fcb402c5f7fefae06ffa03
artemius466 <81032239+artemius466@users.noreply.github.com>
2023-05-01 20:42:43 +0700

    Create GetMyIP.js

---
+++ GetMyIP.js
@@ -0,0 +1,62 @@
+
+
+(function(Scratch) {
+  'use strict';
+  const vm = Scratch.vm;
+
+
+
+
+  class ScratchGetMyIP {
+    constructor() {
+    }
+
+
+
+    getInfo() {
+      return {
+        id: "GetMyIP",
+        name: "Get my IP",
+        color1: '#A33AE5',
+        color2: '#C72FFA',
+        color3: '#CE1EE6',
+        blocks: [
+          {
+            opcode: "GetIP",
+            blockType: "reporter",
+            text: "Get my IP",
+            arguments: {}
+          },
+          {
+            opcode: "GetCountry",
+            blockType: "reporter",
+            text: "Get my country",
+            arguments: {}
+          }
+        ],
+      };
+    }
+
+    GetIP(args) {
+
+      return Scratch.fetch('https://jsonip.com')
+        .then(r => r.json())
+        .then(d => d.ip)
+        .catch(() => '');
+
+    }
+
+    GetCountry(args) {
+      return Scratch.fetch('https://jsonip.com')
+        .then(r => r.json())
+        .then(d => d.country)
+        .catch(() => '');
+    }
+
+
+  }
+
+
+
+  Scratch.extensions.register(new ScratchGetMyIP())
+})(Scratch);
